00b717de2494e8206ffeb482a347ef1c99f4d02d
Husein Causevic <husein.causevic@student.tugraz.at>
2021-12-08 19:23:07 +0100

    add loop example in ejs

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,3 +1,14 @@
 export const getHome = (req, res) => {
-    res.render('../public/views/index.ejs');
+    const data = {
+        title: 'Web Technology 2021',
+        items: [
+            {
+                name: 'item 1',
+            },
+            {
+                name: 'item 2',
+            }
+        ]
+    }
+    res.render('../public/views/index.ejs', data);
 };
